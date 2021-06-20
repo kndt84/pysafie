196de744d8f96ff423afa5f95b51fcd150d0eb93
Takashi Kaneda <takashi.kaneda@futurestandard.co.jp>
2021-06-20 23:42:26 +0900

    Add device APIs

--- pysafie/__init__.py
+++ pysafie/__init__.py
@@ -51,6 +51,94 @@ class Safie:
         self.expires_at = int(time.time()) + d['expires_in']
         return res
 
+    # Device APIs
+    def get_device_image(self, device_id, timestamp=None):
+        url = '{}/v1/devices/{}/image'.format(
+            BASE_URL, device_id)
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        if timestamp == None:
+            return requests.get(url, headers=headers)
+        else:
+            payload = {
+                'timestamp': timestamp.strftime('%Y-%m-%dT%H:%M:%S+0900')
+            }
+            return requests.get(url, headers=headers, data=payload)
+
+    def get_device_thumbnail(self, device_id):
+        url = '{}/v1/devices/{}/thumbnail'.format(
+            BASE_URL, device_id)
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        return requests.get(url, headers=headers)
+
+    def get_device_list(self, offset=0, limit=20, item_id=None):
+        url = '{}/v1/devices'.format(BASE_URL)
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        payload = {
+            'offset': offset,
+            'limit': limit,
+            'item_id': item_id
+        }
+        return requests.get(url, headers=headers, data=payload)
+
+    def register_device_event(self, device_id, event_id, timestamp):
+        url = '{}/v1/devices/{}/events/{}'.format(
+            BASE_URL, device_id, event_id)
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        payload = {
+            'timestamp': timestamp.strftime('%Y-%m-%dT%H:%M:%S+0900')
+        }
+        return requests.get(url, headers=headers, data=payload)
+
+    def get_device_event_list(self, device_id, start_datetime, end_datetime, offset=0, limit=20):
+        start = start_datetime.strftime('%Y-%m-%dT%H:%M:%S+0900')
+        end = end_datetime.strftime('%Y-%m-%dT%H:%M:%S+0900')
+        url = '{}/v1/devices/{}/events'.format(BASE_URL, device_id)
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        payload = {
+            'start': start,
+            'end': end,
+            'offset': offset,
+            'limit': limit
+        }
+        return requests.get(url, headers=headers, data=payload)
+
+    def get_device_media_list(self, device_id, start_datetime, end_datetime):
+        start = start_datetime.strftime('%Y-%m-%dT%H:%M:%S+0900')
+        end = end_datetime.strftime('%Y-%m-%dT%H:%M:%S+0900')
+        url = '{}/v1/devices/{}/media'.format(BASE_URL, device_id)
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        payload = {
+            'start': start,
+            'end': end
+        }
+        return requests.get(url, headers=headers, data=payload)
+
+    def get_device_location(self, device_id):
+        url = '{}/v1/devices/{}/location'.format(BASE_URL, device_id)
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        return requests.get(url, headers=headers)
+
+    def get_device_still_capture(self, device_id):
+        url = '{}/v1/devices/{}/still_capture'.format(BASE_URL, device_id)
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        return requests.post(url, headers=headers)
+
     # Media file APIs
     def get_media_file_request_list(self, device_id):
         url = '{}/v1/devices/{}/media_files/requests'.format(
@@ -72,7 +160,7 @@ class Safie:
             'start': start,
             'end': end
         }
-        return requests.post(url, data=json.dumps(payload), headers=headers)
+        return requests.post(url, headers=headers, data=json.dumps(payload))
 
     def get_media_file_request(self, device_id, request_id):
         url = '{}/v1/devices/{}/media_files/requests/{}'.format(
