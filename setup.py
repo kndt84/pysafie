837d83fdce4a73c0b2daac777379f1eb8ff7453b
Takashi Kaneda <takashi.kaneda@futurestandard.co.jp>
2021-12-22 14:11:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysafie",  # Replace with your own username
-    version="0.0.2",
+    version="0.2",
     install_requires=[
         "requests",
     ],
